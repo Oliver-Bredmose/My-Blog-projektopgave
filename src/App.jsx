c699deb72720148fd5142856296ad3c4f144f865
Oliver-Bredmose <olbr1@elev.techcollege.dk>
2026-06-18 13:33:45 +0200

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Forside from "./Pages/MainPage/MainPage"
 import Artikel from "./Pages/ArticlePage/articlePage"
+import Login from "./Pages/Login/Login"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Forside />} />
         <Route path="/artikel/:id" element={<Artikel />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import styles from "./Navbar.module.scss"
+import { useNavigate } from "react-router-dom"
 
 function Navbar({ categories, setActiveCategory, activeCategory }) {
+  const navigate = useNavigate()
+
   return (
     <nav className={styles.navbar}>
       <span className={styles.logo}>Kroniken</span>
@@ -15,6 +18,7 @@ function Navbar({ categories, setActiveCategory, activeCategory }) {
           </button>
         ))}
       </div>
+      <button className={styles.loginButton} onClick={() => navigate("/Login")}>Opret konto</button>
     </nav>
   )
 }
